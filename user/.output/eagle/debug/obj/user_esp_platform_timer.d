c3ae91d52124aea9ca0de69783a7245cf6845563
xxx <xxx@xx.com>
2017-08-08 10:38:19 -0700

    start

---
+++ user/.output/eagle/debug/obj/user_esp_platform_timer.d
@@ -0,0 +1,24 @@
+.output/eagle/debug/obj/user_esp_platform_timer.o .output/eagle/debug/obj/user_esp_platform_timer.d : user_esp_platform_timer.c \
+ ../../include/ets_sys.h ../../include/c_types.h \
+ ../../include/eagle_soc.h ../../include/os_type.h \
+ ../../include/ets_sys.h ../../include/mem.h ../../include/osapi.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/string.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/newlib.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/config.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/ieeefp.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/features.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/xtensa/config/core-isa.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/reent.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/_types.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_types.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_default_types.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/lock.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stddef.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/cdefs.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/string.h \
+ ../../include/os_type.h ../include/user_config.h \
+ ../../include/user_interface.h ../../include/ip_addr.h \
+ ../../include/queue.h ../../include/spi_flash.h ../../include/gpio.h \
+ ../../include/espconn.h ../include/user_esp_platform.h
---
+++ user/.output/eagle/debug/obj/user_json.d
@@ -0,0 +1,22 @@
+.output/eagle/debug/obj/user_json.o .output/eagle/debug/obj/user_json.d : user_json.c ../../include/ets_sys.h ../../include/c_types.h \
+ ../../include/eagle_soc.h ../../include/osapi.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/string.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/newlib.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/config.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/ieeefp.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/features.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/xtensa/config/core-isa.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/reent.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/_types.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_types.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_default_types.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/lock.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stddef.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/cdefs.h \
+ /home/xxx/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/string.h \
+ ../../include/os_type.h ../../include/ets_sys.h ../include/user_config.h \
+ ../../include/os_type.h ../../include/mem.h ../include/user_json.h \
+ ../../include/json/jsonparse.h ../../include/c_types.h \
+ ../../include/json/json.h ../../include/json/jsontree.h
